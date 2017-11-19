0b0c04226679b265f01b8121f5da6b281780ba3a
polina357 <kagevina69@mail.ru>
2017-11-19 11:04:25 +0200

    Bug fixes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,85 @@
+var score = document.getElementById('counter');
+score.count = 0;
+var gameField = document.getElementById('game');
+var catcher = document.getElementById('catcher');
+var obj = document.getElementById('timer');
+var timerX;
+
+function getCoords(elem) {
+	var box = elem.getBoundingClientRect();
+	return {
+		top: box.top + pageYOffset,
+		left: box.left + pageXOffset,
+		right: box.right
+	};
+}
+
+function random(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
 var start = document.getElementById('startButton');
 start.onclick = function() {
 	setTimeout(timer, 1000);
-	dropBox();
+	var dropInt = setInterval(dropBox, random(700, 2000));
+
+	var stop = document.getElementById('stopButton');
+	stop.onclick = function() {
+		clearInterval(dropInt);
+		clearTimeout(timerX);
+		alert('Your score is ' + score.count);
+		score.innerHTML = 0;
+		score.count = 0;
+		obj.innerHTML = 60;
+	};
 };
 
 function timer() {
-	var obj = document.getElementById('timer');
+
 	obj.innerHTML--;
 
 	if (obj.innerHTML == 0) {
-		alert('Stop');
 		setTimeout(function() {}, 1000);
+		clearInterval(dropBox);
+		clearInterval(timer);
+		alert('Your score is ' + score.count);
 	} else {
-		setTimeout(timer, 1000);
+		timerX = setTimeout(timer, 1000);
 	}
 }
 
-var score = document.getElementById('score');
-var gameField = document.getElementById('game');
-var catcher = document.getElementById('catcher');
+function dropBox() {
+	var left = random(0, gameField.offsetWidth - 30);
+	var thisBox = document.createElement("div");
+	thisBox.className = 'box';
+	thisBox.style.left = left + "px";
+
+	gameField.appendChild(thisBox);
+	move(thisBox);
+}
+
+function move(thisBox) {
+	var coords = getCoords(thisBox);
+	var topPos = coords.top;
+	var leftPos = coords.left;
+	var rightPos = coords.right;
+	thisBox.style.top = topPos + 10 + 'px';
+
+	var catcherCoords = getCoords(catcher);
+	var topC = catcherCoords.top;
+	var leftC = catcherCoords.left;
+	var rightC = catcherCoords.right;
+
+	if (topPos >= (topC - 30) && leftPos >= leftC && rightPos <= rightC) {
+		thisBox.remove();
+		score.count++;
+		score.innerHTML = score.count;
+	} else if (topPos >= (topC + 10)) {
+		thisBox.remove();
+	} else {
+		setTimeout(move, 50, thisBox);
+	}
+}
 catcher.onmousedown = function(e) {
 	var coords = getCoords(catcher);
 	var shiftX = e.pageX - coords.left;
@@ -45,39 +106,3 @@ catcher.onmousedown = function(e) {
 catcher.ondragstart = function() {
 	return false;
 };
-
-function getCoords(elem) {
-	var box = elem.getBoundingClientRect();
-	return {
-		top: box.top + pageYOffset,
-		left: box.left + pageXOffset
-	};
-}
-
-// Generating falling elements
-function random(min, max) {
-	return Math.round(Math.random() * (max - min) + min);
-}
-
-function dropBox() {
-	var length = random(0, gameField.offsetWidth);
-	var thisBox = document.createElement("div");
-	thisBox.className = 'box';
-	thisBox.style.left = length + "px";
-
-	gameField.appendChild(thisBox);
-	move(thisBox);
-}
-
-function move(thisBox) {
-	var coords = getCoords(thisBox);
-		var topPos = coords.top;
-		var leftPos = coords.left;
-		thisBox.style.top = topPos + 15 + 'px';
-		if ((topPos + 15) >= parseInt(catcher.style.top) && leftPos >= parseInt(catcher.style.left)) {
-			thisBox.remove();
-			score.innerHTML = 1;
-		} else {
-			move(thisBox);
-		}
-}
